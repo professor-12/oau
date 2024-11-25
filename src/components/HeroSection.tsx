051e93e1e5b0def486cb60392c9f7ff35cc08ed1
emmanuel <badejoemmanuel805@gmail.com>
2024-11-24 23:51:55 -0800

    mend

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
             <div className="w-[88%] mx-auto relative  max-lg:pt-[8rem]  max-md:pt-[calc(6rem)] items-center  lg:flex justify-between  gap-24">
                 <div className="flex-1 space-y-4">
                     <h2 className="md:text-6xl text-3xl font-semibold max-md:text-center">Obafemi Awolowo University</h2>
-                    <p className="text-[#1e293bb3]  md:text-xl tracking-normal">
+                    <p className="text-[#1e293bb3] max-md:text-center  md:text-xl tracking-normal">
                         Obafemi Awolowo University (OAU) in Ile-Ife, Nigeria, was established in 1961-62 based on recommendations from a 1959 educational commission led by Sir Eric Ashby. The University’s Provisional Council was formally inaugurated on June 8, 1961, with Chief Rotimi Williams as chairman.</p>
                 </div>
 
